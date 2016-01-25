9e62ade1994d4d246f9a616f8cf8d1909b652d4b
ZEmac <zero.lethanh@gmail.com>
2016-01-26 02:25:03 +0900

    laravel5.2 make:auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,73 +17,73 @@ if (isset($_SERVER['SERVER_PORT']) && ($_SERVER['SERVER_PORT'] != '443')) {
 
 
 DB::connection()->enableQueryLog();
+Route::group(['middleware' => 'web'], function () {
+    Route::resource('photo', 'PhotoController');
+    Route::resource('event', 'EventController');
+    Route::resource('event.photo', 'EventPhotoController');
+    Route::resource('event.tag', 'EventTagController');
 
-Route::resource('photo', 'PhotoController');
-Route::resource('event', 'EventController');
-Route::resource('event.photo', 'EventPhotoController');
-Route::resource('event.tag', 'EventTagController');
+    Route::any('allEvents', ['uses' => 'EventController@allEvents']);
+    Route::any('events', ['uses' => 'EventController@allEvents']);
 
-Route::any('allEvents', ['uses' => 'EventController@allEvents']);
-Route::any('events', ['uses' => 'EventController@allEvents']);
+    Route::any('adminEvents', 'EventController@adminEvents');
+    Route::any('sharedEvents', 'EventController@sharedEvents');
 
-Route::any('adminEvents', 'EventController@adminEvents');
-Route::any('sharedEvents', 'EventController@sharedEvents');
-
-Route::resource('user.event', 'UserEventController');
+    Route::resource('user.event', 'UserEventController');
 
 //Route::resource('vue', 'VueController');
 //Route::controller('vue', 'VueController');
-Route::controller('photos', 'PhotoController', [
-    'getUpload' => 'photos.upload'
-]);
-Route::controller('user', 'UserController');
-Route::controller('events', 'EventController', [
-    'anyAdmin' => 'events.admin',
-    'anyShared' => 'events.shared'
-]);
-Route::controller('comments', 'CommentController');
+    Route::controller('photos', 'PhotoController', [
+        'getUpload' => 'photos.upload'
+    ]);
+    Route::controller('user', 'UserController');
+    Route::controller('events', 'EventController', [
+        'anyAdmin' => 'events.admin',
+        'anyShared' => 'events.shared'
+    ]);
+    Route::controller('comments', 'CommentController');
 
-Route::any('/', ['uses' => 'htmlController@index']);
-Route::any('/home', ['uses' => 'htmlController@home']);
+    Route::any('/', ['uses' => 'htmlController@index']);
+    Route::any('/home', ['uses' => 'htmlController@home']);
 //Route::any('/pusher', ['uses' => 'htmlController@pusher']);
-Route::controller('t', 'tController');
+    Route::controller('t', 'tController');
 
-Route::any('user', ['uses' => 'UserController@getInfo']);
-Route::any('u/{id}', ['uses' => 'UserController@id'])->where(['id' => '[0-9]+']);
-Route::controller('u', 'UserController');
+    Route::any('user', ['uses' => 'UserController@getInfo']);
+    Route::any('u/{id}', ['uses' => 'UserController@id'])->where(['id' => '[0-9]+']);
+    Route::controller('u', 'UserController');
 
-Route::controller('auth', 'Auth\AuthController');
-Route::controller('password', 'Auth\PasswordController');
+    Route::controller('auth', 'Auth\AuthController');
+    Route::controller('password', 'Auth\PasswordController');
 
 //sub domain for angular assets
-Route::group(['domain' => 'assets.welapp.net'], function () {
-    /* angular js , css assets
-     * use:  angularController@index
-     */
-    Route::controller('show', 'assetsController');
+    Route::group(['domain' => 'assets.welapp.net'], function () {
+        /* angular js , css assets
+         * use:  angularController@index
+         */
+        Route::controller('show', 'assetsController');
 //    Route::controller('resources', 'assetsController');
-});
-Route::group(['domain' => '{sub}.welapp.net'], function () {
-    /* angular js , css assets
-     * use:  angularController@index
-     */
-    Route::any('lib/angular/{p1?}/{p2?}/{p3?}/{p4?}/{p5?}/{p6?}/{p7?}', ['uses' => 'angularController@index']);
-});
-Route::controller('angular', 'angularController');
-Route::controller('assets', 'assetsController');
+    });
+    Route::group(['domain' => '{sub}.welapp.net'], function () {
+        /* angular js , css assets
+         * use:  angularController@index
+         */
+        Route::any('lib/angular/{p1?}/{p2?}/{p3?}/{p4?}/{p5?}/{p6?}/{p7?}', ['uses' => 'angularController@index']);
+    });
+    Route::controller('angular', 'angularController');
+    Route::controller('assets', 'assetsController');
 
 
-Route::any('token', function () {
-    return csrf_token();
-});
-Route::any('progress', function () {
-    return view('progress');
-});
+    Route::any('token', function () {
+        return csrf_token();
+    });
+    Route::any('progress', function () {
+        return view('progress');
+    });
 
-Route::get('gettoken', function () {
-    return ['X-CSRF-TOKEN' => csrf_token()];
-});
-Route::group(['middleware' => 'web'], function () {
+    Route::get('gettoken', function () {
+        return ['X-CSRF-TOKEN' => csrf_token()];
+    });
+
     Route::auth();
 
     Route::get('/home', 'HomeController@index');
