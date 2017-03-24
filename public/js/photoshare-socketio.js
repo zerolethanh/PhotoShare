dc638a6f85a69cac45284f38d7190be89a85f201
lvt <zero.lethanh@gmail.com>
2017-03-24 15:16:29 +0900

    update photoshare link realtime

--- public/js/photoshare-socketio.js
+++ public/js/photoshare-socketio.js
@@ -2,7 +2,7 @@
  * Created by ZE on 2015/11/11.
  */
 
-var socket = io('https://photoshare.space:6001', {secure: true});
+var socket = io('https://photoshare.link:6001', {secure: true});
 socket.on('has_new_comment', function (data) {
     if (data.event.id == getEventId()) {
         try {
